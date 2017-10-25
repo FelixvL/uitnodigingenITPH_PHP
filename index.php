54afe508553b66b93ae0a9a69c591544c27abb85
FelixvL <digitaleservice@gmail.com>
2017-10-25 13:48:11 +0200

    Bijdrage Derek, controle check

--- index.php
+++ index.php
@@ -1,6 +1,26 @@
 <?php
     $alleNamen = array("Jan Klaassen","Piet Ertsen","Johan de Geert","Rudolf Latens","Joost Remond", "Fredje Karl", "Dolf Meeuwissen");
-
+    $bijdragen = array(122,22,33,0,44,22,122);
+    
+    $lengteEersteArray = count($alleNamen);
+    echo $lengteEersteArray;
+    $lengteTweedeArray = count($bijdragen);
+    echo $lengteTweedeArray;
+    
+    if($lengteTweedeArray == $lengteEersteArray){
+        echo "HET KLOPT";
+    }else{
+        echo "HET KLOPT NIET";
+    }
+    
+    echo "<hr>";
+    echo "<hr>";
+    echo "<hr>";
+    echo "<hr>";
+    
+    
+    
+    
     for($x=0; $x < count($alleNamen); $x++){
         if($alleNamen[$x] == "Johan de Geert"){
             continue;
