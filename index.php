e8ad98fc226259086eecb7c6d5c1f7d6cede8884
FelixvL <digitaleservice@gmail.com>
2017-10-25 14:04:36 +0200

    Bijdrage Anne Marije, bijdrage erbij

--- index.php
+++ index.php
@@ -1,39 +1,23 @@
 <?php
     $alleNamen = array("Jan Klaassen","Piet Ertsen","Johan de Geert","Rudolf Latens","Joost Remond", "Fredje Karl", "Dolf Meeuwissen");
     $bijdragen = array(122,22,33,0,44,22,122);
-    
-    $lengteEersteArray = count($alleNamen);
-    echo $lengteEersteArray;
-    $lengteTweedeArray = count($bijdragen);
-    echo $lengteTweedeArray;
-    
-    if($lengteTweedeArray == $lengteEersteArray){
-        echo "HET KLOPT";
-    }else{
+
+    if(count($alleNamen) != count($bijdragen)){
         echo "HET KLOPT NIET";
     }
-    
-    echo "<hr>";
-    echo "<hr>";
-    echo "<hr>";
-    echo "<hr>";
-    
-    
-    
-    
+        
     for($x=0; $x < count($alleNamen); $x++){
         if($alleNamen[$x] == "Johan de Geert"){
             continue;
         }
-        voorstellen($alleNamen[$x]);            
-        
-     }
-    
-    
-    function voorstellen($naam){
+        voorstellen($alleNamen[$x], $bijdragen[$x]);            
+     }     
+     
+    function voorstellen($naam, $bedrag){
         echo "Welkom ";
         echo $naam;
         echo " op deze kerst borrel";
+        echo "betalen: ".$bedrag;
         echo "<br><br>";
     }
 ?>
