5e47c2b5b77e68871d0545bae61b59817205af68
FelixvL <digitaleservice@gmail.com>
2017-10-25 13:27:56 +0200

    Bijdrage Oday, loop aangemaakt

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php
-    $naam = array("Jan Klaassen","Piet Ertsen","Johan de Geert","Rudolf Latens","Joost Remond");
+    $alleNamen = array("Jan Klaassen","Piet Ertsen","Johan de Geert","Rudolf Latens","Joost Remond");
+
+    for($x=0; $x <= 4; $x++){
+        voorstellen($alleNamen[$x]);
+     }
     
-    voorstellen("Fred Karelsen");
     
     function voorstellen($naam){
         echo "Welkom ";
         echo $naam;
         echo " op deze kerst borrel";
-        
+        echo "<br><br>";
     }
 ?>
