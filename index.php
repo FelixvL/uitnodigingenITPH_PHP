ba8e17efb984cdfd650c09b5acf25ccbae131731
FelixvL <digitaleservice@gmail.com>
2017-10-25 13:16:12 +0200

    Bijdrage Bas, eerste aanroep gemaakt

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
-
-    echo "uitnodigingen";
-
+    $naam = array("Jan Klaassen","Piet Ertsen","Johan de Geert","Rudolf Latens","Joost Remond");
+    
+    voorstellen("Fred Karelsen");
+    
+    function voorstellen($naam){
+        echo "Welkom ";
+        echo $naam;
+        echo " op deze kerst borrel";
+        
+    }
 ?>
